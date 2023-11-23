6953113292fa8408fd8b7a951d8165dcbfdebbc8
Manthan <manthansoul@gmail.com>
2023-11-23 19:41:58 +0530

    App Initialized with Vite

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import { ContextProvider } from "./Context";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ContextProvider>
+);
